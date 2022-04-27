875b5e809e2224e56932e6cfe6ee741ad6ea163f
Alan Villanueva <villanuevayallealan@gmail.com>
2022-04-27 11:15:34 -0500

    "Se modificó el formulario de agregar recepcion y su respectivo controlador"

---
+++ application/views/atencionCliente/recepcion/nuevo.php
@@ -0,0 +1,186 @@
+<div id="netix_form"><br>
+	<form id="formulario" class="form-horizontal" v-on:submit.prevent="netix_guardar()"> 
+		<!--DATOS DEL EMPLEADO QUE ATIENDE-->
+	    <div class="row form-group">
+	    	<div class="col-xs-12" style="display: flex; align-items: center; justify-content: center; ">
+	    		<label style="text-decoration: underline; font-size: 15px;">DATOS DEL EMPLEADO</label>
+	    	</div><br><br>
+
+	    	<div class="col-md-6 col-xs-12">
+                <label>SELECCIONAR SUCURSAL</label>
+                <select class="form-control" name="codsucursal" v-model="campos.codsucursal" required>
+                    <option value="">SELECCIONE</option>
+                    <?php 
+                        foreach ($sucursales as $key => $value) { ?>
+                            <option value="<?php echo $value['codsucursal'];?>"><?php echo $value["descripcion"];?></option>
+                        <?php }
+                    ?>
+                </select>
+            </div>
+            <div class="col-md-4 col-xs-12">
+                <label>NRO DE DNI</label>
+                <input type="text" name="documento" v-model.trim="campos.documento" class="form-control" required autocomplete="off" placeholder="N° DNI . . ." />
+            </div>
+            <div class="col-md-2 col-xs-12" style="padding-top:23px;">
+                <button type="button" class="btn btn-success btn-block btn-consultar" v-on:click="netix_consultar()"> <i class="fa fa-search"></i> </button>
+            </div>
+        </div>
+
+        <div class="row form-group">
+            <div class="col-md-6 col-xs-12">
+                <label>SELECCIONAR AREA</label>
+                <select class="form-control" name="codarea" v-model="campos.codarea" required>
+                    <option value="">SELECCIONE</option>
+                    <?php 
+                        foreach ($areas as $key => $value) { ?>
+                            <option value="<?php echo $value['codarea'];?>"><?php echo $value["descripcion"];?></option>
+                        <?php }
+                    ?>
+                </select>
+            </div>
+            <div class="col-md-6 col-xs-12">
+                <label>SELECCIONAR CARGO</label>
+                <select class="form-control" name="codcargo" v-model="campos.codcargo" required>
+                    <option value="">SELECCIONE</option>
+                    <?php 
+                        foreach ($cargos as $key => $value) { ?>
+                            <option value="<?php echo $value['codcargo'];?>"><?php echo $value["descripcion"];?></option>
+                        <?php }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class="col-xs-12">
+                <label>NOMBRES COMPLETOS</label>
+                <input type="text" name="razonsocial" v-model.trim="campos.razonsocial" class="form-control" required autocomplete="off" placeholder="Nombres completos . . ." />
+            </div>
+        </div>
+
+        <!--DATOS DEL CLIENTE-->
+	    <div class="row form-group">
+	    	<div class="col-xs-12" style="display: flex; align-items: center; justify-content: center; ">
+	    		<label style="text-decoration: underline; font-size: 15px;">DATOS DEL CLIENTE</label>
+	    	</div><br><br>
+
+	    	<div class="col-md-5 col-xs-12">
+		    	<label>TIPO DOCUMENTO</label>
+		    	<select class="form-control" name="coddocumentotipo" v-model="campos.coddocumentotipo" required v-on:change="netix_tipodocumento()" ref="coddocumentotipo">
+		    		<option value="">SELECCIONE</option>
+		            <?php 
+		                foreach ($tipodocumentos as $key => $value) { ?>
+		                    <option value="<?php echo $value['coddocumentotipo'];?>"><?php echo $value["descripcion"];?></option>
+		                <?php }
+		            ?>
+		    	</select>
+		    </div>
+		    <div class="col-md-5 col-xs-12">
+		    	<label>NRO. DOCUMENTO</label>
+	        	<input type="text" class="form-control line-danger" name="documento" v-model="campos.documento" id="documento" placeholder="N° DOCUMENTO" required autocomplete="off" minlength="8" maxlength="15" ref="documento">
+		    </div>
+		    <div class="col-md-2 col-xs-12" style="padding-top:25px;">
+		    	<button type="button" class="btn btn-success btn-block btn-consultar" v-on:click="netix_consultar();"> <i class="fa fa-search"></i> </button>
+		    </div>
+	    </div>
+
+	    <div class="row form-group">
+	    	<div class="col-xs-12">
+                <label>NOMBRES COMPLETOS</label>
+                <input type="text" name="razonsocial" v-model.trim="campos.razonsocial" class="form-control" required autocomplete="off" placeholder="Nombres completos . . ." />
+            </div>
+	    </div>
+	    <div class="row form-group">
+	    	<div class="col-xs-12">
+		        <label>DIRECCIÓN</label>
+		        <input type="text" class="form-control" name="direccion" v-model="campos.direccion" placeholder="Direccion" required autocomplete="off">
+		    </div>
+	    </div>
+	    <div class="row form-group">
+	    	<div class="col-xs-6">
+		        <label>EMAIL</label>
+		        <input type="text" class="form-control" name="email" v-model="campos.email" placeholder="Email" autocomplete="off">
+		    </div>
+
+		    <div class="col-md-6">
+	            <label>TELF./CEL.</label>
+	            <input type="number" class="form-control" name="telefono" v-model="campos.telefono" placeholder="Telf./Cel." autocomplete="off">
+	        </div>
+	    </div>
+	    
+	    <div class="row form-group">
+	        <div class="col-md-6">
+	            <label>DISTRITO</label>
+	            <select class="form-control" name="codubigeo" v-model="campos.codubigeo" id="codubigeo" required>
+	                <option value="">SELECCIONE</option>
+	            </select>
+	        </div>
+	        <div class="col-md-6">
+	            <label>PROVINCIA</label>
+	            <select class="form-control" name="provincia" v-model="campos.provincia" id="provincia" required v-on:change="netix_distritos()">
+	                <option value="">SELECCIONE</option>
+	            </select>
+	        </div>
+	    </div>
+
+	    <div class="row form-group"> 
+            <div class="col-md-6">
+	            <label>DEPARTAMENTO</label>
+	            <select class="form-control" name="departamento" v-model="campos.departamento" required v-on:change="netix_provincias()">
+	                <option value="">SELECCIONE</option>
+	                <?php 
+	                    foreach ($departamentos as $key => $value) { ?>
+	                        <option value="<?php echo $value['ubidepartamento'];?>"><?php echo $value["departamento"];?></option>
+	                    <?php }
+	                ?>
+	            </select>
+	        </div>
+
+	        <div class="col-xs-6">
+	        	<label >FECHA</label>
+	            <input type="date" class="form-control input-sm datepicker" id="fechadesde" value="<?php echo date('Y-m-d');?>" autocomplete="off">
+	        </div>
+	    </div>
+
+        <div class="row form-group">
+        	<div class="col-xs-12" style="display: flex; align-items: center; justify-content: center; ">
+        		<label style="text-decoration: underline; font-size: 15px;">DATOS DEL PRODUCTO</label>
+        	</div><br><br>
+
+            <div class="col-xs-4">
+                <label>PRODUCTO</label>
+                <input type="text" class="form-control" name="descripcion" v-model="#" placeholder="Producto..." autocomplete="off">
+            </div>
+
+            <div class="col-xs-4">
+                <label>MARCA</label>
+                <input type="text" class="form-control" name="marca" v-model="#" placeholder="Marca..." autocomplete="off">
+            </div>
+
+            <div class="col-xs-4">
+                <label>MODELO</label>
+                <input type="text" class="form-control" name="modelo" v-model="#" placeholder="Modelo..." autocomplete="off">
+            </div>
+        </div>
+
+        <div class="row form-group">
+            <div class="col-xs-12">
+                <label>DESCRIPCIÓN DEL PROBLEMA</label>
+                <input type="text" style="height:50px;" class="form-control" name="descripcion" v-model="#" placeholder="Problema..." autocomplete="off">
+            </div>
+        </div>
+
+        <div class="row form-group">
+            <div class="col-xs-12">
+                <label>OBSERVACIONES</label>
+                <input type="text" style="height:50px;" class="form-control" name="observacion" v-model="#" placeholder="Observación..." autocomplete="off">
+            </div>
+        </div>
+
+		<div class="ln_solid"></div>
+		<div class="form-group" align="center">
+			<button type="submit" class="btn btn-success" v-bind:disabled="estado==1"> <i class="fa fa-save"></i> GUARDAR </button>
+			<button type="button" class="btn btn-danger" v-on:click="netix_cerrar()">CERRAR</button>
+		</div>
+	</form> 
+</div>	 
+
