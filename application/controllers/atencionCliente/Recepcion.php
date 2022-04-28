83c0577caa610c47eee0b2986db099a9112987d3
Alejandro Zorrilla <a98z9883@gmail.com>
2022-04-28 09:46:19 -0500

    controller recepcion add function nuevo

--- application/controllers/atencionCliente/Recepcion.php
+++ application/controllers/atencionCliente/Recepcion.php
@@ -4,12 +4,54 @@ class Recepcion extends CI_Controller{
 
     function __construct(){
         parent::__construct();
-        //$this->load->model("Recepcion_Moldel");
+        parent::__construct(); $this->load->model("Netix_model");
     }
     public function index(){
         if ($this->input->is_ajax_request()) {
             if (isset($_SESSION["netix_usuario"])) {
                 $this->load->view("atencionCliente/recepcion/index");
+            }else{
+                $this->load->view("netix/505");
+            }
+        }else{
+            $this->load->view("netix/404");
+        }
+    }
+    public function lista(){
+        if ($this->input->is_ajax_request()) {
+            $this->request = json_decode(file_get_contents('php://input'));
+            $limit = 8; $offset = $this->request->pagina * $limit - $limit;
+
+            $lista = $this->db->query("Select p.razonsocial  as nombrepersona, e.razonsocial  as nombreempleado,r.*
+from public.recepcion r inner join public.personas p on r.codpersona = p.codpersona 
+inner join public.personas e on r.codempleado = e.codpersona offset ".$offset." limit ".$limit)->result_array();
+
+            $total = $this->db->query("select count(*) as total from public.recepcion r inner join public.personas p on r.codpersona = p.codpersona 
+inner join public.personas e on r.codempleado = e.codpersona where (UPPER(p.razonsocial) like UPPER('%".$this->request->buscar."%') or UPPER(e.razonsocial) like UPPER('%".$this->request->buscar."%')) and p.estado=1")->result_array();
+
+            $paginas = floor($total[0]["total"] / $limit);
+            if ( ($total[0]["total"] % $limit)!=0 ) {
+                $paginas = $paginas + 1;
+            }
+
+            $paginacion = array();
+            $paginacion["total"] = $total[0]["total"];
+            $paginacion["actual"] = $this->request->pagina;
+            $paginacion["ultima"] = $paginas;
+            $paginacion["desde"] = $offset;
+            $paginacion["hasta"] = $offset + $limit;
+
+            echo json_encode(array("lista" => $lista,"paginacion" => $paginacion));
+        }else{
+            $this->load->view("netix/404");
+        }
+    }
+    public function nuevo(){
+        if ($this->input->is_ajax_request()) {
+            if (isset($_SESSION["netix_usuario"])) {
+                $empleados = $this->db->query("select persona.codpersona, persona.razonsocial from public.personas as persona inner join public.empleados as empleado on(persona.codpersona=empleado.codpersona) where empleado.estado=1")->result_array();
+                //$cargos = $this->db->query("select * from public.cargos where estado=1")->result_array();
+                $this->load->view("atencionCliente/recepcion/nuevo",compact("empleados"));
             }else{
                 $this->load->view("netix/505");
             }
